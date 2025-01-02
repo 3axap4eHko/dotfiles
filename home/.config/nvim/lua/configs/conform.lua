b30f05d78b02834b86496a766a58534e0ca21ea6
Ivan Zakharchanka <3axap4eHko@gmail.com>
2025-01-02 17:04:22 -0500

    adds formatters

--- home/.config/nvim/lua/configs/conform.lua
+++ home/.config/nvim/lua/configs/conform.lua
@@ -3,13 +3,26 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+
+    javascript = { "deno_fmt" },
+    javascriptreact = { "deno_fmt" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+
+    json = { "prettier" },
+    jsonc = { "prettier" },
+    markdown = { "prettier" },
+    mdx = { "prettier" },
+
+    sh = { "shfmt" },
+    go = { "gofmt" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- home/.config/nvim/lua/configs/vim-tmux-navigator.lua
+++
@@ -1,7 +0,0 @@
-local map = vim.keymap.set
-print("tmux")
-map("n", "<C-A-h>", ":TmuxNavigateLeft<CR>")
-map("n", "<C-A-j>", ":TmuxNavigateDown<CR>")
-map("n", "<C-A-k>", ":TmuxNavigateUp<CR>")
-map("n", "<C-A-l>", ":TmuxNavigateRight<CR>")
-
