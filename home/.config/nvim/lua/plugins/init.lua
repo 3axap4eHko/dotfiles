e8fb2836c9e7b2299aaa13b360b9da561edf582d
Ivan Zakharchanka <3axap4eHko@gmail.com>
2025-01-13 08:36:28 -0500

    fixes init_bash prompt

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -62,6 +62,19 @@ return {
     },
   },
   {
+    "kdheepak/lazygit.nvim",
+     cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    keys = {
+        { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+  {
     "christoomey/vim-tmux-navigator",
     cmd = {
       "TmuxNavigateLeft",
