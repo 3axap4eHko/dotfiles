980867456b0bb2d13c05f66abd69c047eac7d82a
Ivan Zakharchanka <3axap4eHko@gmail.com>
2026-03-17 23:57:43 -0400

    updates statusline

--- mk/ai.mk
+++ mk/ai.mk
@@ -3,6 +3,7 @@
 install-ai:
 	git clone https://github.com/3axap4eHko/ai.git ~/.local/ai/ || git -C ~/.local/ai pull
 	~/.local/ai/install.sh
+	jq -s '.[0] * .[1]' ~/.claude/settings.json home/.claude/statusline.json > /tmp/claude/settings.json && mv /tmp/claude/settings.json ~/.claude/settings.json
 
 save-ai:
-	cp -r ~/.config/ccstatusline/ home/.config/
+	jq '{statusLine}' ~/.claude/settings.json > home/.claude/statusline.json
